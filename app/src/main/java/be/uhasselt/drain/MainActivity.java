126ebd0c0f6b740f8fc1e706dd1ddc7dc730e871
deenr <53797991+deenr@users.noreply.github.com>
2020-04-06 03:12:54 +0200

    Weather included and activity_main fixed

--- app/src/main/java/be/uhasselt/drain/MainActivity.java
+++ app/src/main/java/be/uhasselt/drain/MainActivity.java
@@ -1,8 +1,17 @@
 package be.uhasselt.drain;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +23,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -25,13 +35,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, LocationListener {
 
     private Button btnBottle, btnGlass, btnCan;
-    private TextView txtTodayStats, txtYesterday, txtYesterdayStats, txtWeatherCity, txtWeatherDegrees;
+    private TextView txtTodayStats, txtYesterday, txtYesterdayStats, txtWeather, txtWeatherCity, txtWeatherDegrees;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
@@ -42,6 +61,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private FirebaseDatabase firebaseDatabase;
 
     private DrinkProfile drinkProfile;
+    LocationManager locationManager;
+    String provider;
+    String urlString;
+
+    int MY_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,11 +79,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         btnBottle = (Button) findViewById(R.id.btn_add_bottle);
         btnGlass = (Button) findViewById(R.id.btn_add_glass);
         btnCan = (Button) findViewById(R.id.btn_add_can);
+
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
         txtTodayStats = (TextView) findViewById(R.id.tv_today_stats);
+
         txtYesterday = (TextView) findViewById(R.id.tv_yesterday);
         txtYesterdayStats = (TextView) findViewById(R.id.tv_yesterday_stats);
+
+        txtWeather = (TextView) findViewById(R.id.tv_weather);
         txtWeatherCity = (TextView) findViewById(R.id.tv_weather_city);
         txtWeatherDegrees = (TextView) findViewById(R.id.tv_weather_degrees);
 
@@ -91,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 int dayStart = drinkProfile.getStartDate();
                 int day = (dayNow - dayStart) + 1;
                 drinkProfile.setDay(day);
-                if(drinkProfile.getDrinkList() != null) {
+                if (drinkProfile.getDrinkList() != null) {
                     myRefDrink.setValue(drinkProfile);
                 }
                 updateStats();
@@ -145,6 +173,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(MainActivity.this, "Can added", Toast.LENGTH_SHORT).show();
             }
         });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSION);
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location == null) {
+            Log.e("TAG", "No Location");
+        }
+        setWeather(location);
     }
 
     @Override
@@ -193,7 +237,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void updateStats() {
-        if(drinkProfile.getDrinkList() != null) {
+        if (drinkProfile.getDrinkList() != null) {
             double amountDrankInPercentage = drinkProfile.getAmountDrankInPercentage();
             progressBar.setProgress((int) amountDrankInPercentage);
 
@@ -222,6 +266,130 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 txtYesterday.setVisibility(View.INVISIBLE);
                 txtYesterdayStats.setVisibility(View.INVISIBLE);
             }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSION);
+        }
+
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSION);
+        }
+
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        setWeather(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    class JsonTask extends AsyncTask<Void, Void, String> {
+        String s;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStreamReader streamReader = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+                Log.e("JSON", builder.toString());
+                s = builder.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return s;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                if (s != null) {
+                    JSONObject jsonObject = new JSONObject(s);
+
+                    String city = jsonObject.getString("name");
+                    JSONObject mainObject = jsonObject.getJSONObject("main");
+                    String temp = mainObject.getString("temp");
+                    double tempInteger = Double.parseDouble(temp);
+
+                    txtWeatherCity.setText(city);
+                    txtWeatherDegrees.setText(String.format("%.0f °C", tempInteger));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public String getWeatherURL(double lat, double lng) {
+        return "https://api.openweathermap.org/data/2.5/weather" + String.format("?lat=%s&lon=%s&APPID=%s&units=metric", lat, lng, "874a37d5e38723de1afd44c12d274c3e");
+    }
+
+    public void setWeather(Location location) {
+        if (location == null) {
+            txtWeather.setVisibility(View.INVISIBLE);
+            txtWeatherCity.setVisibility(View.INVISIBLE);
+            txtWeatherDegrees.setVisibility(View.INVISIBLE);
+        } else {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            urlString = getWeatherURL(lat, lng);
+            txtWeather.setVisibility(View.VISIBLE);
+            txtWeatherCity.setVisibility(View.VISIBLE);
+            txtWeatherDegrees.setVisibility(View.VISIBLE);
+            new JsonTask().execute();
         }
     }
 }
